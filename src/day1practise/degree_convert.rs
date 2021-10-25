2490a80ae6b90d68242f9c5b690c685e883fa0f8
Neil Wang <wangweili457@gmail.com>
2021-10-25 16:00:05 +0800

    REFACTORED

--- src/day1practise/degree_convert.rs
+++ src/day1practise/degree_convert.rs
@@ -3,11 +3,11 @@ pub fn degree_convert(input: f64, unit: &str, target_unit: &str) -> String {
     const CELSIUS: f64 = 1.0 / 1.8;
     const CELSIUS_ERROR_VALUE: f64 = -32.0 / 1.8;
     let mut value: f64 = 0.0;
-    if unit.eq("FAHRENHEIT") {
-        value = input / FAHRENHEIT;
+    value = if unit.eq("FAHRENHEIT") {
+        input / FAHRENHEIT
     } else {
-        value = (input - CELSIUS_ERROR_VALUE) / CELSIUS;
-    }
+        (input - CELSIUS_ERROR_VALUE) / CELSIUS
+    };
     return format!("{:.2}", if target_unit.eq("FAHRENHEIT") {
         value * FAHRENHEIT
     } else {
