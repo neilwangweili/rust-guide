c52c790538cddb8539900e20075c778b40babe4c
Neil Wang <wangweili457@gmail.com>
2021-11-17 10:24:56 +0800

    DONE: learn_match.rs

--- src/day5/learn_match.rs
+++ src/day5/learn_match.rs
@@ -2,7 +2,11 @@ use crate::day5::coin::Coin;
 
 pub fn match_coins(coin: Coin) -> i8 {
     match coin {
-        Coin::Penny => 1,
+        Coin::Penny => {
+            // Executable code
+            println!("Lucky penny!");
+            1
+        }
         Coin::Nickel => 5,
         Coin::Dime => 10,
         Coin::Quarter => 25
