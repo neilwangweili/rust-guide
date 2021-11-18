a663808a46684a407f8f53b0d6d229cd00159910
Neil Wang <wangweili457@gmail.com>
2021-11-18 13:50:50 +0800

    DONE

--- src/day6/mod.rs
+++ src/day6/mod.rs
@@ -1,3 +1,5 @@
 pub mod direct_panic;
 pub mod recoverable_panic;
 pub mod read_username_from_file;
+pub mod unimplemented;
+pub mod unreachable;
--- tests/day6.rs
+++ tests/day6.rs
@@ -2,6 +2,8 @@ use std::io::Error;
 use learn_rust::day6::direct_panic::direct_panic;
 use learn_rust::day6::read_username_from_file::read_username_from_file;
 use learn_rust::day6::recoverable_panic::recoverable_panic;
+use learn_rust::day6::unimplemented::unimplemented;
+use learn_rust::day6::unreachable::unreachable;
 
 #[test]
 #[should_panic(expected = "Crash and burn!")]
@@ -22,4 +24,16 @@ pub fn should_panic_directly_with_question_mark() {
         Err(e) => panic!("{}", e),
         _ => {}
     }
+}
+
+#[test]
+#[should_panic(expected = "not implemented")]
+pub fn unimplemented_method() {
+    unimplemented();
+}
+
+#[test]
+#[should_panic(expected = "")]
+pub fn unreachable_method() {
+    unreachable();
 }
