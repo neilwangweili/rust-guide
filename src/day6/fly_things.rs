356dde06429e46176d91f00ddb4a5135dd895a6f
Neil Wang <wangweili457@gmail.com>
2021-11-19 11:36:16 +0800

    DONE: dyn vs impl

--- src/day6/fly_things.rs
+++ src/day6/fly_things.rs
@@ -1,5 +1,7 @@
 use crate::day6::flyable::Flyable;
 
-pub fn fly_things(item: &impl Flyable) -> String {
+// dyn impl ? dyn means fat pointer. impl means item need to impl Flyable.
+pub fn fly_things(item: &dyn Flyable) -> String {
+// pub fn fly_things(item: &impl Flyable) -> String {
     item.fly()
 }
