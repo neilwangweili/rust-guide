6620564eba44b842a56dd6432e6fa95e5a27bcb6
Neil Wang <wangweili457@gmail.com>
2021-11-21 17:36:15 +0800

    DONE: unit_converter.rs 10mm -> 1cm

--- tests/dojo_scaffold/unit_converter.rs
+++ tests/dojo_scaffold/unit_converter.rs
@@ -4,3 +4,8 @@ use learn_rust::dojo_scaffold::unit_converter::unit::Unit;
 fn should_1000_mm_return_1_m() {
     assert_eq!(Unit::new("mm", 1000.0).to("m"), 1.0);
 }
+
+#[test]
+fn should_10_mm_return_1_cm() {
+    assert_eq!(Unit::new("mm", 10.0).to("cm"), 1.0);
+}
