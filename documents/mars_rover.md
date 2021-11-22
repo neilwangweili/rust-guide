eec517b01fe9bb4f2acfab6a97604f35eda766e4
Neil Wang <wangweili457@gmail.com>
2021-11-22 17:41:30 +0800

    DONE: Refactor tests.

--- documents/mars_rover.md
+++ documents/mars_rover.md
@@ -61,7 +61,7 @@
 - [x] should_mars_rover_move_toward_at_east -> "I'm 4 on the X-axis and 3 on the Y-axis and facing East."
 - [x] should_mars_rover_move_toward_at_south -> "I'm 3 on the X-axis and 4 on the Y-axis and facing South."
 - [x] should_mars_rover_move_toward_at_west -> "I'm 2 on the X-axis and 3 on the Y-axis and facing West."
-- [ ] should_mars_rover_move_toward_at_north -> "I'm 3 on the X-axis and 2 on the Y-axis and facing North."
+- [x] should_mars_rover_move_toward_at_north -> "I'm 3 on the X-axis and 2 on the Y-axis and facing North."
 
 **Can move back.**
 - [ ] should_mars_rover_move_back_at_east -> "I'm 2 on the X-axis and 3 on the Y-axis and facing East."
--- tests/dojo/mars_rover.rs
+++ tests/dojo/mars_rover.rs
@@ -1,3 +1,4 @@
+use learn_rust::dojo::mars_rover::command::Command;
 use learn_rust::dojo::mars_rover::direction::Direction;
 use learn_rust::dojo::mars_rover::mars_rover::MarsRover;
 use learn_rust::dojo::mars_rover::run_toward::RunToward;
@@ -6,114 +7,74 @@ use learn_rust::dojo::mars_rover::turn_right::TurnRight;
 
 #[test]
 fn should_init_a_mars_rover_with_x_max_10_y_max_15_x_3_y_3() {
-    assert_eq!(MarsRover::put_on(10, 10, 3, 3, Direction::N)
-                   .report(), "I'm 3 on the X-axis and 3 on the Y-axis and facing North.");
+    assert_eq!(generate_mars_rover_for_test().report(), "I'm 3 on the X-axis and 3 on the Y-axis and facing North.");
 }
 
 #[test]
 fn should_mars_rover_turn_left_to_east_at_north() {
-    let mut mars_rover = MarsRover::put_on(10, 10, 3, 3, Direction::N);
-    let mut vec = Vec::new();
-    vec.push(TurnLeft::new());
-    mars_rover.execute_commands(&vec);
-    assert_eq!(mars_rover.report(), "I'm 3 on the X-axis and 3 on the Y-axis and facing East.");
+    test_template(&mut generate_mars_rover_for_test(), &vec![TurnLeft::new()], "I'm 3 on the X-axis and 3 on the Y-axis and facing East.");
 }
 
 #[test]
 fn should_mars_rover_turn_left_to_south_at_east() {
-    let mut mars_rover = MarsRover::put_on(10, 10, 3, 3, Direction::E);
-    let mut vec = Vec::new();
-    vec.push(TurnLeft::new());
-    mars_rover.execute_commands(&vec);
-    assert_eq!(mars_rover.report(), "I'm 3 on the X-axis and 3 on the Y-axis and facing South.");
+    test_template(&mut generate_mars_rover_for_test(), &vec![TurnLeft::new()], "I'm 3 on the X-axis and 3 on the Y-axis and facing South.");
 }
 
 #[test]
 fn should_mars_rover_turn_left_to_west_at_south() {
-    let mut mars_rover = MarsRover::put_on(10, 10, 3, 3, Direction::S);
-    let mut vec = Vec::new();
-    vec.push(TurnLeft::new());
-    mars_rover.execute_commands(&vec);
-    assert_eq!(mars_rover.report(), "I'm 3 on the X-axis and 3 on the Y-axis and facing West.");
+    test_template(&mut generate_mars_rover_for_test(), &vec![TurnLeft::new()], "I'm 3 on the X-axis and 3 on the Y-axis and facing West.");
 }
 
 #[test]
 fn should_mars_rover_turn_left_to_north_at_west() {
-    let mut mars_rover = MarsRover::put_on(10, 10, 3, 3, Direction::W);
-    let mut vec = Vec::new();
-    vec.push(TurnLeft::new());
-    mars_rover.execute_commands(&vec);
-    assert_eq!(mars_rover.report(), "I'm 3 on the X-axis and 3 on the Y-axis and facing North.");
+    test_template(&mut generate_mars_rover_for_test(), &vec![TurnLeft::new()], "I'm 3 on the X-axis and 3 on the Y-axis and facing North.");
 }
 
 #[test]
 fn should_mars_rover_turn_right_to_west_at_north() {
-    let mut mars_rover = MarsRover::put_on(10, 10, 3, 3, Direction::N);
-    let mut vec = Vec::new();
-    vec.push(TurnRight::new());
-    mars_rover.execute_commands(&vec);
-    assert_eq!(mars_rover.report(), "I'm 3 on the X-axis and 3 on the Y-axis and facing West.");
+    test_template(&mut generate_mars_rover_for_test(), &vec![TurnRight::new()], "I'm 3 on the X-axis and 3 on the Y-axis and facing West.");
 }
 
 #[test]
 fn should_mars_rover_turn_right_to_south_at_west() {
-    let mut mars_rover = MarsRover::put_on(10, 10, 3, 3, Direction::W);
-    let mut vec = Vec::new();
-    vec.push(TurnRight::new());
-    mars_rover.execute_commands(&vec);
-    assert_eq!(mars_rover.report(), "I'm 3 on the X-axis and 3 on the Y-axis and facing South.");
+    test_template(&mut generate_mars_rover_for_test(), &vec![TurnRight::new()], "I'm 3 on the X-axis and 3 on the Y-axis and facing South.");
 }
 
 #[test]
 fn should_mars_rover_turn_right_to_east_at_south() {
-    let mut mars_rover = MarsRover::put_on(10, 10, 3, 3, Direction::S);
-    let mut vec = Vec::new();
-    vec.push(TurnRight::new());
-    mars_rover.execute_commands(&vec);
-    assert_eq!(mars_rover.report(), "I'm 3 on the X-axis and 3 on the Y-axis and facing East.");
+    test_template(&mut generate_mars_rover_for_test(), &vec![TurnRight::new()], "I'm 3 on the X-axis and 3 on the Y-axis and facing East.");
 }
 
 #[test]
 fn should_mars_rover_turn_right_to_north_at_east() {
-    let mut mars_rover = MarsRover::put_on(10, 10, 3, 3, Direction::E);
-    let mut vec = Vec::new();
-    vec.push(TurnRight::new());
-    mars_rover.execute_commands(&vec);
-    assert_eq!(mars_rover.report(), "I'm 3 on the X-axis and 3 on the Y-axis and facing North.");
+    test_template(&mut generate_mars_rover_for_test(), &vec![TurnRight::new()], "I'm 3 on the X-axis and 3 on the Y-axis and facing North.");
 }
 
 #[test]
 fn should_mars_rover_move_toward_at_east() {
-    let mut mars_rover = MarsRover::put_on(10, 10, 3, 3, Direction::E);
-    let mut vec = Vec::new();
-    vec.push(RunToward::new());
-    mars_rover.execute_commands(&vec);
-    assert_eq!(mars_rover.report(), "I'm 4 on the X-axis and 3 on the Y-axis and facing East.");
+    test_template(&mut generate_mars_rover_for_test(), &vec![RunToward::new()], "I'm 4 on the X-axis and 3 on the Y-axis and facing East.");
 }
 
 #[test]
 fn should_mars_rover_move_toward_at_south() {
-    let mut mars_rover = MarsRover::put_on(10, 10, 3, 3, Direction::S);
-    let mut vec = Vec::new();
-    vec.push(RunToward::new());
-    mars_rover.execute_commands(&vec);
-    assert_eq!(mars_rover.report(), "I'm 3 on the X-axis and 4 on the Y-axis and facing South.");
+    test_template(&mut generate_mars_rover_for_test(), &vec![RunToward::new()], "I'm 3 on the X-axis and 4 on the Y-axis and facing South.");
 }
 
 #[test]
 fn should_mars_rover_move_toward_at_west() {
-    let mut mars_rover = MarsRover::put_on(10, 10, 3, 3, Direction::W);
-    let mut vec = Vec::new();
-    vec.push(RunToward::new());
-    mars_rover.execute_commands(&vec);
-    assert_eq!(mars_rover.report(), "I'm 2 on the X-axis and 3 on the Y-axis and facing West.");
+    test_template(&mut generate_mars_rover_for_test(), &vec![RunToward::new()], "I'm 2 on the X-axis and 3 on the Y-axis and facing West.");
 }
 
 #[test]
 fn should_mars_rover_move_toward_at_north() {
-    let mut mars_rover = MarsRover::put_on(10, 10, 3, 3, Direction::N);
-    let mut vec = Vec::new();
-    vec.push(RunToward::new());
-    mars_rover.execute_commands(&vec);
-    assert_eq!(mars_rover.report(), "I'm 3 on the X-axis and 2 on the Y-axis and facing North.");
+    test_template(&mut generate_mars_rover_for_test(), &vec![RunToward::new()], "I'm 3 on the X-axis and 2 on the Y-axis and facing North.");
+}
+
+fn generate_mars_rover_for_test() -> MarsRover {
+    MarsRover::put_on(10, 10, 3, 3, Direction::N)
+}
+
+fn test_template<T: Command>(mars_rover: &mut MarsRover, commands: &Vec<T>, valid_string: &str) {
+    mars_rover.execute_commands(commands);
+    assert_eq!(mars_rover.report(), valid_string);
 }
