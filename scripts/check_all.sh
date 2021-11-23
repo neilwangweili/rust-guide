fbe555e21c63aba13345a7845f05dd88d4c910fb
Neil Wang <wangweili457@gmail.com>
2021-11-23 10:14:42 +0800

    DONE: Add grcov

--- scripts/check_all.sh
+++ scripts/check_all.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
-cargo test
+cargo test --color=always --message-format=json-diagnostic-rendered-ansi --no-run --package rust-guide --test integration_test
+grcov ./target/debug/ -s . -t html --llvm --branch --ignore-not-existing -o ./target/debug/coverage/
 cargo fmt --all
 cargo clippy -- -D warnings
