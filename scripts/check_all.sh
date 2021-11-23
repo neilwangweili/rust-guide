0e47c1214d7e695da5e698096e34d51ddf20f09b
Neil Wang <wangweili457@gmail.com>
2021-11-23 11:54:21 +0800

    DONE: Add grcov

--- scripts/check_all.sh
+++ scripts/check_all.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 
 cargo clean
-cargo test
+cargo +nightly build
+cargo +nightly test -- --test-threads=1
+grcov ./target/debug/ -s . -t html --llvm --branch --ignore-not-existing -o ./target/debug/coverage/
 cargo fmt --all
 cargo clippy -- -D warnings
