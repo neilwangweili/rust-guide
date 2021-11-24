c98224fa1380014b08416b3860357a7904eaaa10
Neil Wang <wangweili457@gmail.com>
2021-11-25 00:18:37 +0800

    DONE: add.rs

---
+++ src/day7/add.rs
@@ -0,0 +1,6 @@
+#[macro_export]
+macro_rules! add {
+    ($a: expr, $b: expr) => {
+        $a + $b
+    };
+}
--- tests/day7.rs
+++ tests/day7.rs
@@ -1,4 +1,4 @@
-use rust_guide::{calculate_result, create_function};
+use rust_guide::{add, calculate_result, create_function};
 
 #[test]
 fn should_create_function_macro_rule_run_correctly() {
@@ -9,4 +9,9 @@ fn should_create_function_macro_rule_run_correctly() {
 #[test]
 fn should_calculate_result_macro_rule_run_correctly() {
     assert_eq!(calculate_result!(5 + 6), 11);
+}
+
+#[test]
+fn should_add_macro_rule_run_correctly() {
+    assert_eq!(add!(1, 2), 3);
 }
