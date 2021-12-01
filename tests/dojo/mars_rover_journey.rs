7902f332dd393869be216919f316b0fa06f32dad
Neil Wang <wangweili457@gmail.com>
2021-12-01 20:45:41 +0800

    DONE: mars_rover.rs should_stop_move_outside_s

--- tests/dojo/mars_rover_journey.rs
+++ tests/dojo/mars_rover_journey.rs
@@ -263,3 +263,15 @@ fn should_stop_move_outside_n() {
         "I'm 5.0 on the X-axis and 0.0 on the Y-axis and facing North."
     );
 }
+
+#[test]
+fn should_stop_move_outside_s() {
+    let mut mars_rover = MarsRover::put_on(5, 5, 5, 5, Direction::S);
+    let mut vec = Vec::new();
+    vec.push(RunToward::make());
+    mars_rover.execute_commands(&vec);
+    assert_eq!(
+        mars_rover.report(),
+        "I'm 5.0 on the X-axis and 5.0 on the Y-axis and facing South."
+    );
+}
