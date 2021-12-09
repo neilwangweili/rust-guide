e8567b20b472d5c7f957bc2ea406b11ca4320407
Neil Wang <wangweili457@gmail.com>
2021-12-09 21:40:58 +0800

    DONE: should_print_grep_line

--- tests/dojo/grep.rs
+++ tests/dojo/grep.rs
@@ -2,10 +2,12 @@ use rust_guide::dojo::grep::grep::Grep;
 
 #[test]
 fn should_init_grep() {
-    assert_eq!(
-        Grep::of("./tests/dojo/file_for_test_grep/test1.txt")
-            .about("hello")
-            .len(),
-        8
-    );
+    let grep = Grep::of("./tests/dojo/file_for_test_grep/test1.txt").about("hello");
+    assert_eq!(grep.len(), 8);
+}
+
+#[test]
+fn should_print_grep_line() {
+    let grep = Grep::of("./tests/dojo/file_for_test_grep/test1.txt").about("hello");
+    assert_eq!(grep[0].show(), "hello 1");
 }
