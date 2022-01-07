be6d7bbd27d7d8d35f8b558f4399752bfc7b530f
Neil Wang <wangweili457@gmail.com>
2022-01-07 11:30:14 +0800

    DONE: workout I2

--- src/day8/workout.rs
+++ src/day8/workout.rs
@@ -2,22 +2,17 @@ use std::thread;
 use std::time::Duration;
 
 pub fn workout(intensity: i32, random_number: i32) -> String {
-    let mut report = String::new();
+    let expensive_closure = |intensity: i32| {
+        println!("Calculating now...");
+        thread::sleep(Duration::from_secs(1));
+        intensity
+    };
     if intensity < 25 {
-        report = format!("Today, do {} push ups!", generate_workout(intensity));
-        report += &format!(" Next, do {} sit ups!", generate_workout(intensity));
+        format!("Today, do {} push ups!", expensive_closure(intensity))
+            + &format!(" Next, do {} sit ups!", expensive_closure(intensity))
+    } else if random_number == 3 {
+        String::from("Take a break today.")
     } else {
-        if random_number == 3 {
-            report = String::from("Take a break today.");
-        } else {
-            report = format!("Today, run {} circles.", generate_workout(intensity));
-        }
+        format!("Today, run {} circles.", expensive_closure(intensity))
     }
-    report
-}
-
-fn generate_workout(intensity: i32) -> i32 {
-    println!("Calculating now...");
-    thread::sleep(Duration::from_secs(1));
-    intensity
 }
