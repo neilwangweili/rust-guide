07538b4a335aa0f81f5c247d8656a50ecf3fa144
Neil Wang <wangweili457@gmail.com>
2022-08-08 10:53:45 +0800

    DONE: Fix.

--- src/day8/add_four.rs
+++ src/day8/add_four.rs
@@ -1,7 +1,7 @@
 pub fn add_four(x: i32) -> i32 {
     let add_one_v1 = |x: i32| -> i32 { x + 1 };
     let add_one_v2 = |x: i32| x + 1;
-    let add_one_v3 = |x| x + 1;
-    let add_one_v4 = |x| x + 1;
+    let add_one_v3 = |x: i32| x + 1;
+    let add_one_v4 = |x: i32| x + 1;
     add_one_v1(add_one_v2(add_one_v3(add_one_v4(x))))
 }
